5d377493744ee57e658c4ac60d1b1d1e10e25cb6
arodriguezq12 <131390056+arodriguezq12@users.noreply.github.com>
2026-02-17 20:09:38 -0600

    Fix MvcApplication type loading by restoring proper namespace/startup

---
+++ Models/ViewModels/FacturaCreateVM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PAV_PF_AlejandroRodriguezQ.Models.ViewModels
+{
+    public class FacturaCreateVM
+    {
+        [Display(Name = "Identificación")]
+        public string IdentificacionUsuario { get; set; }
+
+        [Display(Name = "Exento de impuestos")]
+        public bool EsExento { get; set; }
+
+        public List<FacturaLineaVM> Lineas { get; set; } = new List<FacturaLineaVM>();
+    }
+
+    public class FacturaLineaVM
+    {
+        [Display(Name = "Medicamento")]
+        public string MedicamentoCodigo { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0")]
+        public int Cantidad { get; set; }
+    }
+}
