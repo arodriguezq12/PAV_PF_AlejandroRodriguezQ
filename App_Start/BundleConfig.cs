7521ba1fb57b69dc561878a6ae651cd600c81185
arodriguezq12 <131390056+arodriguezq12@users.noreply.github.com>
2026-02-28 13:20:27 -0600

    Fix CS0266 in BundleConfig helper

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,27 +8,42 @@ namespace PAV_PF_AlejandroRodriguezQ
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            // Disable default bundling transforms (minification) to avoid WebGrease parser crashes with modern JS syntax
+            // Mantener comportamiento por ambiente y evitar minificación de WebGrease en JS.
             BundleTable.EnableOptimizations = false;
 
-            // Include the pre-minified jQuery to avoid runtime minifier crashes with newer jQuery syntax
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-3.7.0.min.js"));
+            bundles.Add(CreateScriptBundleWithoutMinification("~/bundles/jquery",
+                "~/Scripts/jquery-3.7.0.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate.unobtrusive.min.js"));
+            bundles.Add(CreateScriptBundleWithoutMinification("~/bundles/jqueryval",
+                "~/Scripts/jquery.validate.min.js",
+                "~/Scripts/jquery.validate.unobtrusive.min.js"));
 
-            // Use modernizr minified file
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-2.8.3.js"));
+            bundles.Add(CreateScriptBundleWithoutMinification("~/bundles/modernizr",
+                "~/Scripts/modernizr-2.8.3.js"));
 
-            // Use the minified bootstrap bundle (includes Popper) to avoid minification issues
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.bundle.min.js"));
+            bundles.Add(CreateScriptBundleWithoutMinification("~/bundles/bootstrap",
+                "~/Scripts/bootstrap.bundle.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+        }
+
+        private static ScriptBundle CreateScriptBundleWithoutMinification(string virtualPath, params string[] includes)
+        {
+            var bundle = new ScriptBundle(virtualPath);
+            bundle.Include(includes);
+            bundle.Transforms.Clear();
+            bundle.Transforms.Add(new PreserveJsTransform());
+            return bundle;
+        }
+
+        private sealed class PreserveJsTransform : IBundleTransform
+        {
+            public void Process(BundleContext context, BundleResponse response)
+            {
+                response.ContentType = "text/javascript";
+            }
         }
     }
 }
