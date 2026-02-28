4c0989ec427f4f78700aedf8734f20ecb0a827e9
arodriguezq12 <131390056+arodriguezq12@users.noreply.github.com>
2026-02-28 13:11:54 -0600

    Fix login/register interaction by restoring ScriptBundle behavior

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,23 +8,31 @@ namespace PAV_PF_AlejandroRodriguezQ
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            // Disable default bundling transforms (minification) to avoid WebGrease parser crashes with modern JS syntax
+            // Keep framework default (debug/release behavior) and disable only JS minification transforms per bundle.
             BundleTable.EnableOptimizations = false;
 
-            // Include the pre-minified jQuery to avoid runtime minifier crashes with newer jQuery syntax
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-3.7.0.min.js"));
+            var jqueryBundle = new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.7.0.min.js");
+            jqueryBundle.Transforms.Clear();
+            bundles.Add(jqueryBundle);
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate.unobtrusive.min.js"));
+            var jqueryValBundle = new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate.min.js",
+                        "~/Scripts/jquery.validate.unobtrusive.min.js");
+            jqueryValBundle.Transforms.Clear();
+            bundles.Add(jqueryValBundle);
 
-            // Use modernizr minified file
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-2.8.3.js"));
+            // Modernizr can remain as delivered file; avoid WebGrease transforms as well.
+            var modernizrBundle = new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-2.8.3.js");
+            modernizrBundle.Transforms.Clear();
+            bundles.Add(modernizrBundle);
 
-            // Use the minified bootstrap bundle (includes Popper) to avoid minification issues
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.bundle.min.js"));
+            // Use the minified bootstrap bundle (includes Popper) and skip JS transforms.
+            var bootstrapBundle = new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.bundle.min.js");
+            bootstrapBundle.Transforms.Clear();
+            bundles.Add(bootstrapBundle);
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
